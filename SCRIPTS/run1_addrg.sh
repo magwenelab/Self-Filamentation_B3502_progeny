e42dcf3002e37a1c5cfef3407ae52c6756d851c5
Cullen Roth <croth@localhost.localdomain>
2021-02-09 14:35:21 -0700

    Added readgroup adds

---
+++ SCRIPTS/run1_addrg.sh
@@ -0,0 +1,42 @@
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B12_S72_L001-sm.bam -s B12 -r S72.1 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B12_S72_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B12_S72_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B08_S68_L001-sm.bam -s B08 -r S68.5 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B08_S68_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B08_S68_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B04_S64_L001-sm.bam -s B04 -r S64.9 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B04_S64_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B04_S64_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A44_S60_L001-sm.bam -s A44 -r S60.13 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A44_S60_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A44_S60_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A40_S56_L001-sm.bam -s A40 -r S56.17 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A40_S56_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A40_S56_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A36_S52_L001-sm.bam -s A36 -r S52.21 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A36_S52_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A36_S52_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A32_S48_L001-sm.bam -s A32 -r S48.25 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A32_S48_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A32_S48_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A28_S44_L001-sm.bam -s A28 -r S44.29 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A28_S44_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A28_S44_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A24_S40_L001-sm.bam -s A24 -r S40.33 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A24_S40_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A24_S40_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A20_S36_L001-sm.bam -s A20 -r S36.37 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A20_S36_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A20_S36_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A16_S32_L001-sm.bam -s A16 -r S32.41 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A16_S32_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A16_S32_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A12_S28_L001-sm.bam -s A12 -r S28.45 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A12_S28_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A12_S28_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A08_S24_L001-sm.bam -s A08 -r S24.49 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A08_S24_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A08_S24_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A04_S20_L001-sm.bam -s A04 -r S20.53 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A04_S20_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A04_S20_L001-sm.bam
+
---
+++ SCRIPTS/run3_addrg.sh
@@ -0,0 +1,42 @@
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B10_S70_L001-sm.bam -s B10 -r S70.3 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B10_S70_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B10_S70_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B06_S66_L001-sm.bam -s B06 -r S66.7 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B06_S66_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B06_S66_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B02_S62_L001-sm.bam -s B02 -r S62.11 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B02_S62_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B02_S62_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A42_S58_L001-sm.bam -s A42 -r S58.15 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A42_S58_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A42_S58_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A38_S54_L001-sm.bam -s A38 -r S54.19 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A38_S54_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A38_S54_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A34_S50_L001-sm.bam -s A34 -r S50.23 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A34_S50_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A34_S50_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A30_S46_L001-sm.bam -s A30 -r S46.27 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A30_S46_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A30_S46_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A26_S42_L001-sm.bam -s A26 -r S42.31 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A26_S42_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A26_S42_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A22_S38_L001-sm.bam -s A22 -r S38.35 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A22_S38_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A22_S38_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A18_S34_L001-sm.bam -s A18 -r S34.39 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A18_S34_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A18_S34_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A14_S30_L001-sm.bam -s A14 -r S30.43 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A14_S30_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A14_S30_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A10_S26_L001-sm.bam -s A10 -r S26.47 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A10_S26_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A10_S26_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A06_S22_L001-sm.bam -s A06 -r S22.51 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A06_S22_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A06_S22_L001-sm.bam
+
+/home/croth/bin/./bamaddrg -b /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A02_S18_L001-sm.bam -s A02 -r S18.55 > /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A02_S18_L001-sm-rg.bam
+gzip /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A02_S18_L001-sm.bam
+
