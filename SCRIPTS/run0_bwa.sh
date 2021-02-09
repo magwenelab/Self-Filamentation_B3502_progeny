a70de78290a77e9182bce04d25b03afa57ba03c4
Cullen Roth <croth@localhost.localdomain>
2021-02-09 09:56:34 -0700

    Added initial alignment scripts

---
+++ SCRIPTS/run0_bwa.sh
@@ -0,0 +1,30 @@
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_H99_03_S9_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_H99_03_S9_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_H99_03_S9_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_B09_S69_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_B09_S69_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B09_S69_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_B05_S65_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_B05_S65_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B05_S65_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_B01_S61_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_B01_S61_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_B01_S61_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A41_S57_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A41_S57_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A41_S57_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A37_S53_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A37_S53_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A37_S53_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A33_S49_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A33_S49_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A33_S49_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A29_S45_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A29_S45_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A29_S45_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A25_S41_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A25_S41_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A25_S41_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A21_S37_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A21_S37_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A21_S37_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A17_S33_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A17_S33_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A17_S33_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A13_S29_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A13_S29_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A13_S29_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A09_S25_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A09_S25_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A09_S25_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A05_S21_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A05_S21_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A05_S21_L001-sm.bam
+
+bwa mem -a -M /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta /analysis/CROTH/SELFFILAM/SS_20201123_A01_S17_L001_R1_001.fastq.gz /analysis/CROTH/SELFFILAM/SS_20201123_A01_S17_L001_R2_001.fastq.gz | samtools view -F 4 -b | samtools sort -o /analysis/CROTH/SELFFILAM/SAM/SS_20201123_A01_S17_L001-sm.bam
+
