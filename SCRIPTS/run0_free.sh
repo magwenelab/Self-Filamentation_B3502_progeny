9e19d462c671d3b90bf1afc852bb3ed40cee5a56
Cullen Roth <croth@localhost.localdomain>
2021-09-04 12:56:50 -0600

    Removed all scripts

--- SCRIPTS/run0_free.sh
+++
@@ -1,9 +0,0 @@
-freebayes -p 1 -r AE017341.1 -f /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta -Z -L /analysis/CROTH/Self-Filamentation_B3502_progeny/listobams.txt | bgzip > /analysis/CROTH/SELFFILAM/VCF/AE017341.1.B3502.progeny.vcf.gz
-
-freebayes -p 1 -r AE017347.1 -f /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta -Z -L /analysis/CROTH/Self-Filamentation_B3502_progeny/listobams.txt | bgzip > /analysis/CROTH/SELFFILAM/VCF/AE017347.1.B3502.progeny.vcf.gz
-
-freebayes -p 1 -r AE017349.1 -f /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta -Z -L /analysis/CROTH/Self-Filamentation_B3502_progeny/listobams.txt | bgzip > /analysis/CROTH/SELFFILAM/VCF/AE017349.1.B3502.progeny.vcf.gz
-
-freebayes -p 1 -r AE017353.1 -f /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta -Z -L /analysis/CROTH/Self-Filamentation_B3502_progeny/listobams.txt | bgzip > /analysis/CROTH/SELFFILAM/VCF/AE017353.1.B3502.progeny.vcf.gz
-
-freebayes -p 1 -r AE017356.1 -f /analysis/CROTH/Self-Filamentation_B3502_progeny/REF/FungiDB-48_CneoformansJEC21_Genome.fasta -Z -L /analysis/CROTH/Self-Filamentation_B3502_progeny/listobams.txt | bgzip > /analysis/CROTH/SELFFILAM/VCF/AE017356.1.B3502.progeny.vcf.gz
